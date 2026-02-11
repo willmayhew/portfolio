def85b940a013a742f1e30663231c3b235517ca0
wm4g21 <wm4g21@soton.ac.uk>
2026-02-11 00:09:56 +0000

    Fix process undefined in browser build

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,13 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import process from 'process';
+
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection()], })
   .catch(err => console.error(err));
+
+(window as any).process = process;
